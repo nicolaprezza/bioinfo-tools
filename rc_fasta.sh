8af43bcb5e2fc65f495139a2295d44d0caaff7e2
Nicola Prezza <nicola.prezza>
2018-04-09 11:15:12 +0200

    faster rc_fasta

--- rc_fasta.sh
+++ rc_fasta.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
 #reverse complement all sequences in input fasta files
-#appends "_RC" to read names 
+#requires seqtk installed (https://github.com/lh3/seqtk)
 
-file=$1
-
-while read line; do
-
-	if [ `echo $line | grep -c "^>"` -eq 0 ]; then
-		
-		echo $line |rev | tr ACGT TGCA
-
-	else
-		printf ${line}"_RC\n"
-	fi
-
-done < $file
+seqtk seq -r $1 
