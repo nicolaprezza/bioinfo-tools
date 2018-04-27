59525d639f87b756cba95b81114e0d17a735e801
nicola <nicolapr@gmail.com>
2018-04-27 14:04:56 +0200

    filter_vcf_snp_fast.sh

--- filter_vcf_snp_fast.sh
+++ filter_vcf_snp_fast.sh
@@ -10,11 +10,10 @@
 nr=$2
 col=$((9+nr))
 
-#first part of header
-zcat $1 | head -n 10000 | grep ^## > h1
+#header
+zcat $1 | head -n 10000 | grep ^# | cut -f 1,2,3,4,5,6,7,8,9,$col > h
 
-#second part of header
-zcat $1 | head -n 10000 | grep ^# | cut -f 1,2,3,4,5,6,7,8,9,$col > h2
+zcat $1 | awk -v col=$col '(((col == "0|1") || (col == "1|0") || (col == "1|1")) && (($4 == "A") || ($4 == "C") || ($4 == "G") || ($4 == "T")) && (($5 == "A") || ($5 == "C") || ($5 == "G") || ($5 == "T")))' | cut -f 1,2,3,4,5,6,7,8,9,$col > body
 
-zcat $1 | awk -v col=$col '(((col == "0|1") || (col == "1|0") || (col == "1|1")) && (($4 == "A") || ($4 == "C") || ($4 == "G") || ($4 == "T")) && (($5 == "A") || ($5 == "C") || ($5 == "G") || ($5 == "T")))' | cut -f 1,2,3,4,5,6,7,8,9,$col
+cat h body
 
