fd9cb7bcf6f4975d75e38d5bdef25674b98bbd24
nicola <nicolapr@gmail.com>
2018-04-27 14:00:09 +0200

    filter_vcf_snp_fast.sh

--- filter_vcf_snp_fast.sh
+++ filter_vcf_snp_fast.sh
@@ -1,5 +1,5 @@
 # input: vcf.gz containing several individuals, individual number (1,2,3 ...)
-# stdout: vcf of the individual with only SNPs and only one allele mutated (0|1 or 1|0)
+# stdout: vcf of the individual with only SNPs
 
 # assumptions: the first 9 columns contain SNP info. The other columns are the individuals.
 
@@ -16,4 +16,5 @@ zcat $1 | head -n 10000 | grep ^## > h1
 #second part of header
 zcat $1 | head -n 10000 | grep ^# | cut -f 1,2,3,4,5,6,7,8,9,$col > h2
 
-zcat $1 | awk -v col=$col '(((col == "0|1") || (col == "1|0")) && (($4 == "A") || ($4 == "C") || ($4 == "G") || ($4 == "T")) && (($5 == "A") || ($5 == "C") || ($5 == "G") || ($5 == "T")))' | cut -f 1,2,3,4,5,6,7,8,9,$col
+zcat $1 | awk -v col=$col '(((col == "0|1") || (col == "1|0") || (col == "1|1")) && (($4 == "A") || ($4 == "C") || ($4 == "G") || ($4 == "T")) && (($5 == "A") || ($5 == "C") || ($5 == "G") || ($5 == "T")))' | cut -f 1,2,3,4,5,6,7,8,9,$col
+
