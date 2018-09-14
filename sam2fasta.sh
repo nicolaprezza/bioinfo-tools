7e6d65026d902a2fcce72f3d607ac8b0047b7d03
Nicola Prezza <nprezza@carpa.di.unipi.it>
2018-09-14 09:14:40 +0200

    added support for gzipped files in sam2fast*

--- sam2fasta.sh
+++ sam2fasta.sh
@@ -1,2 +1,6 @@
-#convert sam to fastq. Name format: position_flag
-cat $1 | grep -v ^@ | awk '{print ">"$4"_"$2"\n"$10}'
+#convert sam to fast. Name format: position_flag
+if [[ $1 =~ \.gz$ ]]; then
+	zcat $1 | grep -v ^@ | awk '{print ">"$4"_"$2"\n"$10}'
+else
+	cat $1 | grep -v ^@ | awk '{print ">"$4"_"$2"\n"$10}'
+fi
--- sam2fastq.sh
+++ sam2fastq.sh
@@ -1,2 +1,6 @@
 #convert sam to fastq. Name format: position_flag
-cat $1 | grep -v ^@ | awk '{print "@"$4"_"$2"\n"$10"\n+\n"$11}'
+if [[ $1 =~ \.gz$ ]]; then
+	zcat $1 | grep -v ^@ | awk '{print "@"$4"_"$2"\n"$10"\n+\n"$11}'
+else
+	cat $1 | grep -v ^@ | awk '{print "@"$4"_"$2"\n"$10"\n+\n"$11}'
+fi
