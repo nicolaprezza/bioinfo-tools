696546221da77602de2e4bfff02c6b2169597f0b
Nicola Prezza <nprezza@carpa.di.unipi.it>
2018-09-21 11:14:49 +0200

    filter_vcf_snp_fast<

--- filter_vcf_snp_fast.sh
+++ filter_vcf_snp_fast.sh
@@ -1,19 +1,20 @@
-# input: vcf.gz containing several individuals, individual number (1,2,3 ...)
-# stdout: vcf of the individual with only SNPs
+# input: vcf.gz containing several individuals and column number containing  individual of interest
+# stdout: vcf of the individual
 
 # assumptions: the first 9 columns contain SNP info. The other columns are the individuals.
 
-# filter_vcf.sh in.vcf.gz indiv_nr
+# filter_vcf.sh in.vcf.gz col_nr
 
 
 
 nr=$2
-col=$((9+nr))
+col=$nr
 
 #header
 zcat $1 | head -n 10000 | grep ^# | cut -f 1,2,3,4,5,6,7,8,9,$col > h
 
-zcat $1 | awk -v col=$col '((($col == "0|1") || ($col == "1|0") || ($col == "1|1")) && (($4 == "A") || ($4 == "C") || ($4 == "G") || ($4 == "T")) && (($5 == "A") || ($5 == "C") || ($5 == "G") || ($5 == "T")))' | cut -f 1,2,3,4,5,6,7,8,9,$col > body
+zcat $1 | awk -v col=$col '(($col == "0|1") || ($col == "1|0") || ($col == "1|1"))' | cut -f 1,2,3,4,5,6,7,8,9,$col > body
 
 cat h body
 
+rm h body
