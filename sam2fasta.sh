e4a64d3f66f71374f27ed7dce3e7213175bd595d
Nicola Prezza <nprezza@carpa.di.unipi.it>
2019-01-11 13:43:57 +0100

    sam2fast: no compressed input files

--- sam2fasta.sh
+++ sam2fasta.sh
@@ -1,6 +1,2 @@
 #convert sam to fast. Name format: position_flag
-if [[ $1 =~ \.gz$ ]]; then
-	zcat $1 | grep -v ^@ | awk '{print ">"$4"_"$2"\n"$10}'
-else
-	cat $1 | grep -v ^@ | awk '{print ">"$4"_"$2"\n"$10}'
-fi
+cat $1 | grep -v ^@ | awk '{print ">"$4"_"$2"\n"$10}'
--- sam2fastq.sh
+++ sam2fastq.sh
@@ -1,6 +1,3 @@
 #convert sam to fastq. Name format: position_flag
-if [[ $1 =~ \.gz$ ]]; then
-	zcat $1 | grep -v ^@ | awk '{print "@"$4"_"$2"\n"$10"\n+\n"$11}'
-else
-	cat $1 | grep -v ^@ | awk '{print "@"$4"_"$2"\n"$10"\n+\n"$11}'
-fi
+cat $1 | grep -v ^@ | awk '{print "@"$4"_"$2"\n"$10"\n+\n"$11}'
+
